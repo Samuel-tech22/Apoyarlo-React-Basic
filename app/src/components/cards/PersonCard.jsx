6718e9baa810bf875cd7a54a42ac5d867feb1f3f
Samuel Candia <scandia022@gmail.com>
2023-12-17 17:07:55 -0300

    Update PersonCard.jsx
    agregar botón y función para aumentar la edad de los participantes

--- app/src/components/cards/PersonCard.jsx
+++ app/src/components/cards/PersonCard.jsx
@@ -1,16 +1,33 @@
-import {Component} from "react";
+import { Component } from "react";
 import style from "./PersonCard.module.css";
 
 class PersonCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            edad: Number(this.props.age)
+        };
+    }
+
+    birthday = () => {
+        this.setState((prevState) => ({
+            edad: prevState.edad += 1
+        }));
+    }
+
     render() {
 
-        const {firstName, lastName, age, hairColor} = this.props;
+        const { firstName, lastName, hairColor } = this.props;
 
-        return(
+        const { edad } = this.state;
+
+        return (
             <div className={style.card}>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Edad: {age}</p>
+                <p>Edad: {edad}</p>
                 <p>Color Cabello: {hairColor}</p>
+                <button onClick= { this.birthday }>Birthday Button For: {lastName}</button>
             </div>
         )
     }
